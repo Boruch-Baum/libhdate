a62036e8d246795ff8dfe45ed58f8b935e0d65cf
kzamir <kzamir@55ca9751-b80e-42d2-876c-1d9b9c78a646>
2005-03-11 01:05:26 +0000

    perl default namspace is hdatec

--- libhdate/examples/bindings/hdate_perl.pl
+++ libhdate/examples/bindings/hdate_perl.pl
@@ -4,23 +4,23 @@
 use hdate;
 
 #
-# !! SOME SWIG VERSIONS WILL CREATE hdatec NAMESPACE !!
+# !! SOME SWIG VERSIONS WILL CREATE hdatec NAMESPACE OTHERS hdate !!
 #
 # use hdate; # just hdate
 #
-# $h = hdatec::new_Hdate();
-# print hdatec::Hdate_get_format_date ($h, 0, 0) . "\n"; # use hdatec !
+# $h = hdate::new_Hdate();
+# print hdatec:Hdate_get_format_date ($h, 0, 0) . "\n"; # use hdate !
 #
 
 # create a new Hdate object
-$h = hdate::new_Hdate();
+$h = hdatec::new_Hdate();
 
 # print gregorian date
 print "Today is:\n";
-print hdate::Hdate_get_gday($h) . "," . hdate::Hdate_get_gmonth($h) . "," . hdate::Hdate_get_gyear($h) . "\n";
+print hdatec::Hdate_get_gday($h) . "," . hdatec::Hdate_get_gmonth($h) . "," . hdatec::Hdate_get_gyear($h) . "\n";
 
 # print hebrew date: 0 - israely holidays, 0 - long format
-print hdate::Hdate_get_format_date ($h, 0, 0) . "\n";
+print hdatec::Hdate_get_format_date ($h, 0, 0) . "\n";
 
 # delete the hdate object
-hdate::delete_Hdate($h);
+hdatec::delete_Hdate($h);
