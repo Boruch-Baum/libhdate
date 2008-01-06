ad5a7afc28acae075e5e5cea0ee86b55a8029cc2
kzamir <kzamir@55ca9751-b80e-42d2-876c-1d9b9c78a646>
2008-01-06 04:54:15 +0000

    update ruby example

--- libhdate/examples/bindings/hdate_python.py
+++ libhdate/examples/bindings/hdate_python.py
@@ -4,11 +4,11 @@
 from hdate import *;
 
 # Create a new Hdate object
-h = Hdate();
+h = Hdate ();
 
 # Print gregorian date
 print "Today is:"
-print str (h.get_gday ()) + "," + str (h.get_gmonth()) + "," + str (h.get_gyear ())
+print str (h.get_gday ()) + "," + str (h.get_gmonth ()) + "," + str (h.get_gyear ())
 
 # Print hebrew date: 0 - long format
 print h.get_format_date (0);
--- libhdate/examples/bindings/hdate_ruby.rb
+++ libhdate/examples/bindings/hdate_ruby.rb
@@ -6,5 +6,9 @@ require 'hdate'
 # Create a new Hdate object
 h = Hdate::Hdate.new
 
+# Print gregorian date
+puts "Today is:"
+puts "#{h.get_gday()},#{h.get_gmonth()},#{h.get_gyear()}"
+
 # Print hebrew date: 0 - long format
 puts h.get_format_date(0);
